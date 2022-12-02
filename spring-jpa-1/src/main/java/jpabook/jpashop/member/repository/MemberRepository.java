a23e54467c438298b3c00ae0b824c3c9ca2d3fe2
ddmkim94 <ddmkim94@gmail.com>
2022-12-02 14:45:18 +0900

    Member entity, repository  추가 후 JPA 동작 확인

---
+++ spring-jpa-1/src/main/java/jpabook/jpashop/member/repository/MemberRepository.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop.member.repository;
+
+import jpabook.jpashop.member.entity.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long memberId) {
+        return em.find(Member.class, memberId);
+    }
+}
