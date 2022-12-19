626a0c9e14867aee1590748f5f66bd5f2ba540fa
ddmkim94 <ddmkim94@gmail.com>
2022-12-19 18:10:09 +0900

    홈화면 마크업

---
+++ spring-jpa-1/src/main/java/jpabook/jpashop/base/controller/HomeController.java
@@ -0,0 +1,17 @@
+package jpabook.jpashop.base.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        log.info("home controller");
+        return "home";
+    }
+}
+
