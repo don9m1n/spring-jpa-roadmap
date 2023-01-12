b2d0ffbe7934c177b27947f209dfe632efec52a0
ddmkim94 <ddmkim94@gmail.com>
2023-01-12 17:50:24 +0900

    순수 JPA로 조회 메서드 생성 & 테스트

--- data-jpa/src/main/java/study/datajpa/member/repository/MemberJpaRepository.java
+++ data-jpa/src/main/java/study/datajpa/member/repository/MemberJpaRepository.java
@@ -40,4 +40,12 @@ public class MemberJpaRepository {
     public Member find(Long id) {
         return em.find(Member.class, id);
     }
+
+    public List<Member> findByUsernameAndAgeGreaterThan(String username, int age) {
+        return em.createQuery("select m from Member m where m.username = :username" +
+                " and m.age > :age", Member.class)
+                .setParameter("username", username)
+                .setParameter("age", age)
+                .getResultList();
+    }
 }
