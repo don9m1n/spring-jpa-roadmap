8830e0530e062f0577020f3dd73562afd2319c8c
Dongmin Kim <ddmkim94@gmail.com>
2023-07-25 12:30:26 +0900

    집합 (group by)

--- querydsl/src/test/java/com/study/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/com/study/querydsl/QuerydslBasicTest.java
@@ -1,9 +1,11 @@
 package com.study.querydsl;
 
 import com.querydsl.core.QueryResults;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.study.querydsl.entity.Member;
 import com.study.querydsl.entity.QMember;
+import com.study.querydsl.entity.QTeam;
 import com.study.querydsl.entity.Team;
 import jakarta.persistence.EntityManager;
 import org.assertj.core.api.Assertions;
@@ -16,6 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 import static com.study.querydsl.entity.QMember.member;
+import static com.study.querydsl.entity.QTeam.team;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -173,4 +176,99 @@ public class QuerydslBasicTest {
 
         assertThat(members.size()).isEqualTo(2);
     }
+
+    @Test
+    void aggregation() {
+        List<Tuple> result = query
+                .select(
+                        member.count(),
+                        member.age.sum(),
+                        member.age.avg(),
+                        member.age.max(),
+                        member.age.min()
+                )
+                .from(member)
+                .fetch();
+
+        Tuple tuple = result.get(0);
+        assertThat(tuple.get(member.count())).isEqualTo(4);
+        assertThat(tuple.get(member.age.sum())).isEqualTo(100);
+        assertThat(tuple.get(member.age.avg())).isEqualTo(25);
+        assertThat(tuple.get(member.age.min())).isEqualTo(10);
+    }
+
+    @Test
+    void group1() {
+        // 팀의 이름과 각 팀의 평균 연령을 구해라.
+        /**
+         * select t.name, avg(m.age)
+         * from member m
+         * inner join team t
+         * on (m.team_id = t.id)
+         * group by t.name;
+         */
+        List<Tuple> result = query
+                .select(team.name, member.age.avg())
+                .from(member)
+                .join(member.team, team)
+                .groupBy(team.name)
+                .fetch();
+
+        Tuple teamA = result.get(0);
+        Tuple teamB = result.get(1);
+
+        assertThat(teamA.get(team.name)).isEqualTo("teamA");
+        assertThat(teamA.get(member.age.avg())).isEqualTo(15);
+
+        assertThat(teamB.get(team.name)).isEqualTo("teamB");
+        assertThat(teamB.get(member.age.avg())).isEqualTo(35);
+    }
+
+    @Test
+    void group2() {
+        // 팀의 이름과 각 팀의 평균 연령을 구해라. (teamB만)
+        /**
+         * select t.name, avg(m.age)
+         * from member m
+         * inner join team t
+         * on (m.team_id = t.id)
+         * group by t.name
+         */
+        List<Tuple> result = query
+                .select(team.name, member.age.avg())
+                .from(member)
+                .join(member.team, team)
+                .groupBy(team.name)
+                .having(team.name.eq("teamB"))
+                .fetch();
+
+        Tuple teamB = result.get(0);
+
+        assertThat(teamB.get(team.name)).isEqualTo("teamB");
+        assertThat(teamB.get(member.age.avg())).isEqualTo(35);
+    }
+
+    @Test
+    void group3() {
+        /**
+         * select t.name, avg(m.age)
+         * from member m
+         * inner join team t
+         * on (m.team_id = t.id)
+         * group by t.name
+         */
+        List<Tuple> result = query
+                .select(team.name, member.age.avg())
+                .from(member)
+                .join(member.team, team)
+                .groupBy(team.name)
+                .having(member.age.avg().gt(30))
+                .fetch();
+
+        Tuple teamB = result.get(0);
+
+        assertThat(teamB.get(team.name)).isEqualTo("teamB");
+        assertThat(teamB.get(member.age.avg())).isEqualTo(35);
+    }
+
 }
